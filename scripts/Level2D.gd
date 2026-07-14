3683fe7cfc91aff23eedf94e1f580b4c66996022
SeniorPinata28 <115035394+SeniorPinata28@users.noreply.github.com>
2026-07-14 18:57:43 +0700

    Add mobile controls and restart wiring

--- scripts/Level2D.gd
+++ scripts/Level2D.gd
@@ -2,6 +2,10 @@ extends Node2D
 
 @onready var player = $Player2D
 @onready var exit_area = $Exit
+@onready var left_button = $MobileUI/LeftButton
+@onready var right_button = $MobileUI/RightButton
+@onready var jump_button = $MobileUI/JumpButton
+@onready var restart_button = $MobileUI/RestartButton
 
 const START_POSITION = Vector2(120, 360)
 
@@ -9,6 +13,18 @@ func _ready():
 	_remove_defeated_enemies()
 	_place_player()
 	exit_area.body_entered.connect(_on_exit_body_entered)
+	left_button.button_down.connect(func(): Input.action_press("ui_left"))
+	left_button.button_up.connect(func(): Input.action_release("ui_left"))
+	right_button.button_down.connect(func(): Input.action_press("ui_right"))
+	right_button.button_up.connect(func(): Input.action_release("ui_right"))
+	jump_button.pressed.connect(func(): Input.action_press("ui_accept"))
+	jump_button.button_up.connect(func(): Input.action_release("ui_accept"))
+	restart_button.pressed.connect(GameManager.reset_run)
+
+func _exit_tree():
+	Input.action_release("ui_left")
+	Input.action_release("ui_right")
+	Input.action_release("ui_accept")
 
 func _place_player():
 	if GameManager.returning_from_duel:
